b25180f0b6c55ada87594f8c4570d326114785f1
Jorge Aguilar <jaguilar@safeglobal.cl>
2021-06-25 16:45:42 -0400

    se agrego modulos de gifs, search y resultados

---
+++ src/app/gifs/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styles: [
+  ]
+})
+export class SearchComponent  {
+
+  buscar( termino: string){
+    
+      console.log(termino);
+    
+  }
+
+
+}
+
+
