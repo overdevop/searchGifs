251dacefe63af7e90986a8808ed0d247ec5175f7
Jorge Aguilar <jaguilar@safeglobal.cl>
2021-06-26 11:07:53 -0400

    Agregado tipado a las peticiones Http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,7 @@ export class GifsService {
   private apiKey      : string = 'uX1wDywugizwSBgATgx5AInarP7J97y0';
   private _historial  : string[] = [];
 
-  // TODO: Cambiar any por su tipo correspondiente //
-  public resultados: any [] = [];
+  public resultados: Gif [] = [];
 
   get historial (){
     
@@ -28,8 +28,8 @@ export class GifsService {
       this._historial = this._historial.splice(0,10);
     }
 
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=uX1wDywugizwSBgATgx5AInarP7J97y0&q=${query}&limit=10`)
-      .subscribe ((respuesta:any) =>{
+    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=uX1wDywugizwSBgATgx5AInarP7J97y0&q=${query}&limit=10`)
+      .subscribe ((respuesta) =>{
         console.log(respuesta.data)
         this.resultados = respuesta.data;
       })
