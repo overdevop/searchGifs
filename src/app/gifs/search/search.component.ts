1344f5fabd52f6cf8d1debfca77f1a647c3fbd30
Jorge Aguilar <jaguilar@safeglobal.cl>
2021-06-26 10:03:40 -0400

    Controlado las busquedas

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -16,6 +16,10 @@ export class SearchComponent  {
   buscar(){
     
     const valor = this.txtBuscar.nativeElement.value;
+
+    if (valor.trim().length === 0){
+      return;
+    }
   
     this.gifsService.buscarGifs(valor);
 
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,11 +8,19 @@ export class GifsService {
   private _historial: string[] = [];
 
   get historial (){
+    
     return [...this._historial];
   }
 
   buscarGifs( query: string) {
-    this._historial.unshift(query);
+    
+    query = query.trim().toLocaleUpperCase();
+
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10);
+    }
+
     console.log(this._historial)
   }
 
