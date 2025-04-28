d5af8c13b828afe912b4127010f986f5c4f71301
Jorge Aguilar <jorgeaguilar@backupcode.cl>
2025-04-28 13:20:24 -0400

    Backup antes de actualizar Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    SharedModule,
-    GifsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        SharedModule,
+        GifsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
